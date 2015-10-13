017b0dd54851a8da349b828fa9e77d086286c707
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-13 16:10:23 -0700

    Load the viewlet into the module

--- gs/group/messages/post/base/__init__.py
+++ gs/group/messages/post/base/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals, print_function
 from zope.i18nmessageid import MessageFactory
 GSMessageFactory = MessageFactory('gs.group.messages.post')
+
+#lint:disable
+from .viewlet import PostViewlet
+#lint:enable
