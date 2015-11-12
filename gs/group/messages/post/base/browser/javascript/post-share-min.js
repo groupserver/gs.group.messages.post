d7366a54cc211294d0d426595d1dc5a790a998ae
Michael JasonSmith <mpj17@onlinegroups.net>
2015-11-12 14:51:17 -0800

    Minifying the Share code, and upping the version number for the JS

--- gs/group/messages/post/base/browser/javascript/post-share-min.js
+++ gs/group/messages/post/base/browser/javascript/post-share-min.js
@@ -1,4 +1,1 @@
-"use strict";jQuery.noConflict();function gs_group_messages_post_share_init(){var b=false,a=null;
-jQuery(".gs-content-js-share").each(function(){b=Boolean(Number(jQuery(this).attr("public")));
-a=GSShareBox(this,b);a.init()})}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-sharebox-min-20140327.js",gs_group_messages_post_share_init)
-});
+'use strict';jQuery.noConflict();function gs_group_messages_post_share_init(){var a=!1,b=null;jQuery(".gs-content-js-share").each(function(){a=Boolean(Number(jQuery(this).attr("public")));b=GSShareBox(this,a);b.init()})}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-sharebox-min-20151112.js",gs_group_messages_post_share_init)});
